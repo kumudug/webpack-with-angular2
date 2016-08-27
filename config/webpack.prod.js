d5cd9e443421629888bd9f73b4f17c1b428667ab
Kumudu Gunasekara <Kumudu Gunasekara>
2016-08-28 01:12:06 +1000

    webpack prod config

---
+++ config/webpack.prod.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+
+  htmlLoader: {
+    minimize: false // workaround for ng2
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(ENV)
+      }
+    })
+  ]
+});
+
+//We don't use a development server. We're expected to deploy the application and its dependencies to a real production server. This time the output bundle files are physically placed in the dist folder.
+//NoErrorsPlugin - stops the build if there is any error.
+//DedupePlugin - detects identical (and nearly identical) files and removes them from the output.
+//UglifyJsPlugin - minifies the bundles.
+//ExtractTextPlugin - extracts embedded css as external files, adding cache-busting hash to the filename.
+//DefinePlugin - use to define environment variables that we can reference within our application.
+/*Thanks to the DefinePlugin and the ENV variable defined at top, we can enable Angular 2 production mode like this:
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}*/
--- steps.md
+++ steps.md
@@ -1,2 +1,8 @@
 -Run npm install to install the packages
     -npm install
+-To run the dev build 
+    -npm start [the url for dev is configured as http://localhost:8080/]
+-To build the prod
+    -npm run build [build is saved at dist folder]
+    -If you open the dist folder using visual studio as a website it should run properly
+    
