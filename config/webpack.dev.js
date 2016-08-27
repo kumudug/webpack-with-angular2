40c81e1e2a4636610c3dc075e99fbe2fb499771b
Kumudu Gunasekara <Kumudu Gunasekara>
2016-08-28 02:41:03 +1000

    karma test

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -24,3 +24,7 @@ module.exports = webpackMerge(commonConfig, {
 });
 
 //The development build relies on the Webpack development server which we configure near the bottom of the file. Although we tell Webpack to put output bundles in the dist folder, the dev server keeps all bundles in memory; it doesn't write them to disk. So we won't find any files in the dist folder
+
+//The HtmlWebpackPlugin (added in webpack.common.js) use the publicPath and the filename settings to generate appropriate <script> and <link> tags into the index.html
+
+//Our CSS are buried inside our Javascript bundles by default. The ExtractTextPlugin extracts them into external .css files that the HtmlWebpackPlugin inscribes as <link> tags into the index.html
--- steps.md
+++ steps.md
@@ -5,4 +5,5 @@
 -To build the prod
     -npm run build [build is saved at dist folder]
     -If you open the dist folder using visual studio as a website it should run properly
-    
+-To run the karma test
+    -npm test
