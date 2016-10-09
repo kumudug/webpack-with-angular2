42dc2f17557fea479df019335054788e0fe605e7
kumudug <kumudug1@gmail.com>
2016-10-09 12:38:11 +1100

    update from rc5 to 2.0

--- config/karma-test-shim.js
+++ config/karma-test-shim.js
@@ -3,11 +3,11 @@
 Error.stackTraceLimit = Infinity;
 
 require('core-js/es6');
-require('reflect-metadata');
+require('core-js/es7/reflect');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
-//require('zone.js/dist/proxy');
+require('zone.js/dist/proxy');
 require('zone.js/dist/sync-test');
 require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,3 +1,5 @@
+//Webpack is a NodeJS-based tool so its configuration is a JavaScript commonjs module file that begins with require statements as such files do.
+
 //We will define separate configurations for development, production, and test environments. All three have some configuration in common. We'll gather that common configuration in a separate file called webpack.common.js.
 
 //Webpack is a NodeJS-based tool so its configuration is a JavaScript commonjs module file that begins with require statements as such files do.
@@ -17,48 +19,48 @@ module.exports = {
     //But most of our import statements won't mention the extension at all. So we tell Webpack to resolve module file requests by looking for matching files with
     resolve: {
         extensions: ['', //an explicit extension
-        '.js', //.js extension
-        '.ts'] //.ts extension
+            '.js', //.js extension
+            '.ts'] //.ts extension
     },
 
     //Webpack can bundle any kind of file: JavaScript, TypeScript, CSS, SASS, LESS, images, html, fonts, whatever. Webpack itself doesn't know what to do with a non-JavaScript file. We teach it to process such files into JavaScript with loaders. Here we configure loaders for TypeScript and CSS:
     module: {
         loaders: [
-        {
-            test: /\.ts$/,
-            loaders: ['ts', //a loader to transpile our Typescript code to ES5, guided by the tsconfig.json file
-            'angular2-template-loader'] //loads angular components' template and styles
-        },
-        {
-            test: /\.html$/,
-            loader: 'html'
-        },
-        {
-            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-            loader: 'file?name=assets/[name].[hash].[ext]'
-        },
-        {
-            test: /\.css$/,
-            exclude: helpers.root('src', 'app'), //application-wide styles. excludes .css files within the /src/app directories where our component-scoped styles sit. It includes only .css files located at or above /src; these are the application-wide styles
-            loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-        },
-        {
-            test: /\.css$/,
-            include: helpers.root('src', 'app'), //filters for component-scoped styles and loads them as strings via the raw loader — which is what Angular expects to do with styles specified in a styleUrls metadata property.
-            loader: 'raw'
-        }
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', //a loader to transpile our Typescript code to ES5, guided by the tsconfig.json file
+                    'angular2-template-loader'] //loads angular components' template and styles
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'), //application-wide styles. excludes .css files within the /src/app directories where our component-scoped styles sit. It includes only .css files located at or above /src; these are the application-wide styles
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'), //filters for component-scoped styles and loads them as strings via the raw loader — which is what Angular expects to do with styles specified in a styleUrls metadata property.
+                loader: 'raw'
+            }
         ]
     },
 
     //Webpack has a build pipeline with well-defined phases. We tap into that pipeline with plugins such as the uglify minification plugin:
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
-        name: ['app', 'vendor', 'polyfills']
+            name: ['app', 'vendor', 'polyfills']
         }),
         //We want the app.js bundle to contain only app code and the vendor.js bundle to contain only the vendor code. Our application code imports vendor code. Webpack is not smart enough to keep the vendor code out of the app.js bundle. We rely on the CommonsChunkPlugin to do that job.
 
         new HtmlWebpackPlugin({
-        template: 'src/index.html'
+            template: 'src/index.html'
         })
         //Webpack generates a number of js and css files. We could insert them into our index.html manually. That would be tedious and error-prone. Webpack can inject those scripts and links for us with the HtmlWebpackPlugin.
     ]
